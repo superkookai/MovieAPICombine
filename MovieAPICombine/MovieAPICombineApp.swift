4d576008c4dd63a4195959f3ca7d2ff2586b21db
superkookai <weerawut@live.com>
2025-03-07 15:57:19 +0700

    Initial Commit

---
+++ MovieAPICombine/MovieAPICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPICombineApp.swift
+//  MovieAPICombine
+//
+//  Created by Weerawut Chaiyasomboon on 07/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct MovieAPICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
